e0f7806e24601443d7e6327723a7dc28e1c03e0a
Shay Highgate <highgateshay@yahoo.com>
2025-08-02 16:46:01 -0500

    Added styles and grid

--- src/main.js
+++ src/main.js
@@ -1,24 +1,28 @@
+
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
+  <main>
+    <header>
+      <h2>Heading</h2>
+    </header>
+
+    <section class="source">
+      <div class="display-div">
+        <img id="display" src="/src/assets/display.png" />
+         <p>Source</p>
+      </div>
+    </section>
+
+    <section class="page-screen">
+      <h2>Page screen</h2>
+    </section>
+
+    <footer>
+      <h2>Footer</h2>
+    </footer>
+  </main>
 `
 
 setupCounter(document.querySelector('#counter'))
